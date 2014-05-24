ed1b916875e683aa88fec1cf039793c9ac1585f1
Adrian-Costin Tundrea <adrian.tundrea@endava.com>
2014-05-24 19:44:42 +0300

    Add partial process

--- src/ro/endava/hackathon/Main.java
+++ src/ro/endava/hackathon/Main.java
@@ -11,32 +11,66 @@ import ro.endava.hackathon.util.Filter;
 
 public class Main {
 	public static void main(String[] args) {
+		// De citit din XML
+		Integer hours = 0;
 		List<ProcessActivity> processActivities = new ArrayList<ProcessActivity>();
 		List<ProcessPerson> processPersons = new ArrayList<ProcessPerson>();
-		System.out.println("Asignare persoane la activitati...");
-		Filter.getOpenActivitiesAndSort(processActivities, Comparators.activityCompareByTicket);
-		for (ProcessActivity processActivity:processActivities) {
-			List<ProcessPerson> availableProcessPersons = Filter.getAvailablePersonForActivityAndSort(processPersons, processActivity, Comparators.personCompareByRemainingBuget);
-			
-			if (availableProcessPersons.size()>processActivity.getActivity().getMinNoOfParticipants()) {
-				System.out.println("Se adauga persoanele la activitatea " + processActivity.getActivity().getName() + "...");
-				if (availableProcessPersons.size()<processActivity.getActivity().getCapacity()) {
-					processActivity.getPersonsAttending().addAll(availableProcessPersons);
-				} else {
-					for (Iterator<ProcessPerson> iterator = availableProcessPersons.listIterator(); iterator.hasNext(); ) {
-					    if (availableProcessPersons.indexOf(iterator.next())+1>processActivity.getActivity().getCapacity()) {
-					    	iterator.remove();
-					    }
+
+		Integer totalTurnover = 0;
+		for (Integer currentHour = 0; currentHour < hours; currentHour++) {
+			System.out.println("Asignare pentru ora " + currentHour + "...");
+			Filter.getOpenActivitiesAndSort(processActivities, Comparators.activityCompareByTicket);
+
+			Integer turnoverThisHour = 0;
+			for (ProcessActivity processActivity : processActivities) {
+				List<ProcessPerson> availableProcessPersons = Filter.getAvailablePersonForActivityAndSort(processPersons, processActivity.getActivity(), Comparators.personCompareByRemainingBuget);
+
+				if (availableProcessPersons.size() > processActivity.getActivity().getMinNoOfParticipants()) {
+					System.out.println("Se adauga persoanele la activitatea " + processActivity.getActivity().getName() + "...");
+					if (availableProcessPersons.size() < processActivity.getActivity().getCapacity()) {
+						processActivity.getPersonsAttending().addAll(availableProcessPersons);
+
+						turnoverThisHour += processActivity.getActivity().getTicketPrice() * availableProcessPersons.size();
+					} else {
+						for (Iterator<ProcessPerson> iterator = availableProcessPersons.listIterator(); iterator.hasNext();) {
+							if (availableProcessPersons.indexOf(iterator.next()) + 1 > processActivity.getActivity().getCapacity()) {
+								iterator.remove();
+							}
+						}
+
+						turnoverThisHour += processActivity.getActivity().getTicketPrice() * processActivity.getActivity().getCapacity();
+					}
+					for (ProcessPerson processPerson : availableProcessPersons) {
+						processPerson.setRemainingBudget(processPerson.getRemainingBudget() - processActivity.getActivity().getTicketPrice());
+						processPerson.setAssigned(true);
+						processPerson.setAssignedToProcessActivity(processActivity);
 					}
 				}
-				for (ProcessPerson processPerson:availableProcessPersons) {
-					processPerson.setAssigned(true);
-					processPerson.setAssignedToProcessActivity(processActivity);
-				}
 			}
-			
-			for () {
+
+			totalTurnover += turnoverThisHour;
+
+			// Scriere output
+
+			for (ProcessActivity processActivity : processActivities) {
+				if (processActivity.getRemainingHours() > 1) {
+					processActivity.setRemainingHours(processActivity.getRemainingHours() - 1);
+				} else {
+					processActivity.setWorking(!processActivity.getWorking());
+					processActivity.setRemainingHours(processActivity.getWorking() ? processActivity.getActivity().getContinuousOpenHours() : processActivity.getActivity().getMaintenanceHours());
+				}
+				processActivity.setPersonsAttending(null);
+			}
+			for (ProcessPerson processPerson : processPersons) {
+				if (processPerson.getRemainingHours() > 1) {
+					processPerson.setRemainingHours(processPerson.getRemainingHours() - 1);
+				} else {
+					processPerson.setAwake(!processPerson.getAwake());
+					processPerson.setRemainingHours(processPerson.getAwake() ? processPerson.getPerson().getMaxAwakeTime() : processPerson.getPerson().getContinuousSleepTime());
+				}
+				processPerson.setAssigned(false);
+				processPerson.setAssignedToProcessActivity(null);
+			}
 		}
-		
 	}
 }
