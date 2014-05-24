9ab5fa566a2acb08d85776d65f2a5ad902c122c6
Alexandru Vasile <alexandru.vasile@endava.com>
2014-05-24 20:32:10 +0300

    write output.xml

--- src/ro/endava/hackathon/util/OutputTransform.java
+++ src/ro/endava/hackathon/util/OutputTransform.java
@@ -3,8 +3,22 @@ package ro.endava.hackathon.util;
 import ro.endava.hackathon.core.ProcessPerson;
 import ro.endava.hackathon.core.Result;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class OutputTransform {
 
@@ -19,5 +33,60 @@ public class OutputTransform {
 			r.setIndex(index);
 		}
 		return res;
+	}
+	
+	public static void writeResult(List<Result> results, Long totalAmount) {
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
+			
+			Element rootElement = doc.createElement("output");
+			Attr xmlnsAttr = doc.createAttribute("xmlns");
+			xmlnsAttr.setValue("http://www.endava.com/hackathon/journey");
+			rootElement.setAttributeNode(xmlnsAttr);
+			Attr teamAttr = doc.createAttribute("teamId");
+			teamAttr.setValue("byteMe");
+			rootElement.setAttributeNode(teamAttr);
+			doc.appendChild(rootElement);
+			
+			Element amount = doc.createElement("totalAmountSpent");
+			amount.appendChild(doc.createTextNode(totalAmount.toString()));
+			rootElement.appendChild(amount);
+			
+			for (Result r : results) {
+				appendEntry(doc, rootElement, r);
+			}
+			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("C:\\output.xml"));
+			transformer.transform(source, result);
+			
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		}
+	}
+
+	private static void appendEntry(Document doc, Element rootElement, Result r) {
+		Element entry = doc.createElement("entry");
+		
+		Element personId = doc.createElement("personId");
+		personId.appendChild(doc.createTextNode(r.getPerson().getName()));
+		entry.appendChild(personId);
+		
+		Element hourIndex = doc.createElement("hourIndex");
+		hourIndex.appendChild(doc.createTextNode(r.getIndex().toString()));
+		entry.appendChild(hourIndex);
+		
+		if (r.getActivity() != null) {
+			Element activity = doc.createElement("activity");
+			activity.appendChild(doc.createTextNode(r.getActivity().getName()));
+		}
+		
+		rootElement.appendChild(entry);
 	}
 }
