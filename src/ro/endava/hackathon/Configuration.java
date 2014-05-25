5a5c4496bd6f45c5956cbebe092e08a5174d317f
gnaftanaila <george.naftanaila@endava.com>
2014-05-25 07:54:26 +0300

    Big data testing

--- src/ro/endava/hackathon/Configuration.java
+++ src/ro/endava/hackathon/Configuration.java
@@ -5,6 +5,7 @@ public class Configuration {
 	public static Boolean TIMER = true;
 	
 	public static String DATASET_PATH = "D:\\dataset.xml";
+	public static String DATASET_PATH_2 = "D:\\dataset2.xml";
 	public static String ADDITION_FILES_PATH = "D:\\additional-files";
 	public static String OUTPUT_PATH = "D:\\output.xml";
 }
